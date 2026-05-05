509af395d1de9ba41bc5cc6c20ef521075c2efec
Connor Garey <connorgarey1999@gmail.com>
2026-05-05 12:55:03 +0100

    Initial Commit.
    .
    
    .
    
    .
    
    .

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Music Scales flake.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    appProperties = {
+      name = "msc";
+      version = "1.0";
+    };
+  in 
+  {
+    packages.x86_64-linux = {
+      debug = pkgs.stdenv.mkDerivation (finalAttrs: {
+        pname = appProperties.name;
+        version = appProperties.version;
+        src = ./.;
+
+        dontStrip = true;
+
+        nativeBuildInputs = with pkgs; [
+          cmake
+          ninja
+        ];
+        buildInputs = [ ];
+      });
+    };
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [ 
+        clang-tools
+        cmake
+        ninja
+      ];
+
+      shellHook = ''
+        echo "Entering shell!"
+      '';
+    };
+  };
+}
