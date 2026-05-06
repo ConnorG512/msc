3ea0d6a6d7eaee58430994727a35616c921ff9e0
Connor Garey <connorgarey1999@gmail.com>
2026-05-06 12:35:07 +0100

    Printing circle of fiths sharps

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "music.hpp"
 
+#include <array>
 #include <print>
 
 int main()
@@ -7,13 +8,24 @@ int main()
   static constexpr auto c_major{Music::Key<>{}};
   static constexpr auto d_major{Music::Key{2, "Major"}};
   static constexpr auto e_major{Music::Key{4, "Major"}};
-  static constexpr auto cs_major{
-      Music::Key{1, "Major", {"B#", "C#", "D", "D#", "E", "E#", "F#", "G", "G#", "A", "A#", "B"}, {2, 2, 1, 2, 2, 2, 1}}};
+  static constexpr auto cs_major{Music::Key{
+      1, "Major", {"B#", "C#", "D", "D#", "E", "E#", "F#", "G", "G#", "A", "A#", "B"}, {2, 2, 1, 2, 2, 2, 1}}};
 
-  std::println("{:s}", Music::generate_title_and_notes(c_major));
-  std::println("{:s}", Music::generate_title_and_notes(d_major));
-  std::println("{:s}", Music::generate_title_and_notes(e_major));
-  std::println("{:s}", Music::generate_title_and_notes(cs_major));
+  static constexpr auto circle_of_fiths = std::to_array({
+      Music::generate_title_and_notes(Music::Key{}),
+      Music::generate_title_and_notes(Music::Key{7, "Major"}),
+      Music::generate_title_and_notes(Music::Key{2, "Major"}),
+      Music::generate_title_and_notes(Music::Key{9, "Major"}),
+      Music::generate_title_and_notes(Music::Key{4, "Major"}),
+      Music::generate_title_and_notes(Music::Key{11, "Major"}),
+      Music::generate_title_and_notes(Music::Key{6, "Major"}),
+      Music::generate_title_and_notes(
+          Music::Key{1, "Major", {"B#", "C#", "D", "D#", "E", "E#", "F#", "G", "G#", "A", "A#", "B"}}),
+  });
 
+  for (const auto &key : circle_of_fiths)
+  {
+    std::println("{:s}", key);
+  }
   return 0;
 }
