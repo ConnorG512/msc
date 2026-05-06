d80a540520dc825b026f88fa315c3fa5bbb77dce
Connor Garey <connorgarey1999@gmail.com>
2026-05-06 10:27:19 +0100

    Created a user-defined deduction guide for CTAD.
    .

--- include/music.hpp
+++ include/music.hpp
@@ -28,6 +28,12 @@ public:
   std::int8_t starting_interval{0};
 };
 
+// User defined CTAD:
+template <std::size_t chromatic_scale_size, std::size_t interval_size>
+Key(std::int8_t, std::string_view, std::array<std::array<char, 4>, chromatic_scale_size>,
+    std::array<std::int8_t, interval_size>) -> Key<chromatic_scale_size, interval_size>;
+
+// Outer Interface:
 template <std::size_t chromatic_scale_size = 12, std::size_t interval_size = 7>
 consteval std::array<char, 16> generate_title(const Key<chromatic_scale_size, interval_size> &key);
 
