69de17fdbac47e60e3f6a1099f413b2fe5e53496
Connor Garey <connorgarey1999@gmail.com>
2026-05-06 13:07:37 +0100

    Added Flat circle of fiths.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "music.hpp"
 
+#include "chromatic-scales.hpp"
+
 #include <array>
 #include <print>
 
@@ -14,6 +16,11 @@ int main()
       Music::generate_title_and_notes(Music::Key{Music::Tonic::F_SHARP, "Major"}),
       Music::generate_title_and_notes(
           Music::Key{Music::Tonic::C_SHARP, "Major", {"B#", "C#", "D", "D#", "E", "E#", "F#", "G", "G#", "A", "A#", "B"}}),
+      Music::generate_title_and_notes(Music::Key{Music::Tonic::F, "Major", Music::ChromaticScales::standard_flat}),
+      Music::generate_title_and_notes(Music::Key{Music::Tonic::B_FLAT, "Major", Music::ChromaticScales::standard_flat}),
+      Music::generate_title_and_notes(Music::Key{Music::Tonic::E_FLAT, "Major", Music::ChromaticScales::standard_flat}),
+      Music::generate_title_and_notes(Music::Key{Music::Tonic::A_FLAT, "Major", Music::ChromaticScales::standard_flat}),
+      Music::generate_title_and_notes(Music::Key{Music::Tonic::D_FLAT, "Major", Music::ChromaticScales::standard_flat}),
   });
 
   for (const auto &key : circle_of_fiths)
