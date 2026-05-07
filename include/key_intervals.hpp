a5317748bca9e4b2c401c57f35885010b1d479a5
Connor Garey <connorgarey1999@gmail.com>
2026-05-07 22:25:08 +0100

    Added const to std::int8_t

--- include/key_intervals.hpp
+++ include/key_intervals.hpp
@@ -4,6 +4,6 @@
 #include <cstdint>
 
 namespace MSC::KeyIntervals {
-  inline constexpr std::array<std::int8_t, 7> Major {2,2,1,2,2,2,1};
-  inline constexpr std::array<std::int8_t, 7> Minor {2,1,2,2,1,2,2};
+  inline constexpr std::array<const std::int8_t, 7> Major {2,2,1,2,2,2,1};
+  inline constexpr std::array<const std::int8_t, 7> Minor {2,1,2,2,1,2,2};
 }
