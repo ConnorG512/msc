3132e07bc8e0eb69b8c96d9d6e93cfc13af4d9f7
Connor Garey <connorgarey1999@gmail.com>
2026-05-08 16:41:29 +0100

    Formatting.

--- src/search.cpp
+++ src/search.cpp
@@ -142,8 +142,10 @@ void MSC::search(const std::size_t scale_hash_input, const std::size_t tonic_has
       MSC::generate_hash("major"),
       MSC::generate_hash("minor"),
   };
-  
-  auto return_scale_value = [](const std::span<const MSC::SearchTable> search_table, const std::size_t scale_hash_input, const std::size_t tonic_hash_input) -> std::string_view {
+
+  auto return_scale_value = [](const std::span<const MSC::SearchTable> search_table, const std::size_t scale_hash_input,
+                               const std::size_t tonic_hash_input) -> std::string_view
+  {
     const auto tonic_result = std::ranges::find_if(search_table, [&tonic_hash_input](const MSC::SearchTable &key)
                                                    { return key.arg_hash == tonic_hash_input; });
     if (tonic_result == search_table.end())
