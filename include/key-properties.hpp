03d13bc71f19f41aeb22d002df9117a8fb01283e
Connor Garey <connorgarey1999@gmail.com>
2026-05-09 15:52:19 +0100

    Moved to a single file.

--- include/key-properties.hpp
+++ include/key-properties.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <cstddef>
 #include <cstdint>
+#include <functional>
 #include <string_view>
 
 namespace MSC::Key
@@ -11,14 +13,23 @@ template <std::size_t interval_size> class Properties
 {
 public:
   template <typename... Args>
-  consteval Properties(std::string_view name, Args... args);
+  consteval Properties(std::string_view name, Args... args)
+    :name_(name), intervals_{static_cast<std::int8_t>(args)...} {
+      if(std::ranges::fold_left(intervals_, 0, std::plus<>()) != 12)
+      {
+        throw "Intervals do not add up to 12 tone scale!";
+      }
+    } 
 
-  std::string_view name_{"Major"};
+  std::string_view name_{};
   std::array<std::int8_t, interval_size> intervals_{};
 };
 
 template <typename... Args>
 Properties(std::string_view, Args...) -> Properties<sizeof...(Args)>;
 
-#include "key-properties.ipp"
 } // namespace MSC::Key
+
+namespace MSC::Key {
+  inline constexpr Properties major{"Major", 2,2,1,2,2,2,1};
+}
