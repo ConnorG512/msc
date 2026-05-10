5f53dcdf76c28fe5c75a99680b1612a0a794eabd
Connor Garey <connorgarey1999@gmail.com>
2026-05-10 14:39:17 +0100

    Added Phrygian and added other modes in enum class.

--- src/search.cpp
+++ src/search.cpp
@@ -240,9 +240,16 @@ void MSC::search(const std::uint64_t scale_hash_input, const std::uint64_t tonic
 {
   enum class Scales
   {
-    Major = 0,
+    Ionian = 0,
     Dorian = 1,
-    Minor = 2,
+    Phrygian = 2,
+    Lydian = 3,
+    Mixolydian = 4,
+    Aeolian = 5,
+    Locrian = 6,
+    
+    Major = 0,
+    Minor = 5,
   };
   static constexpr std::array scale_hashes{
       MSC::generate_hash("ionian"),  MSC::generate_hash("dorian"),     MSC::generate_hash("phrygian"),
@@ -267,6 +274,8 @@ void MSC::search(const std::uint64_t scale_hash_input, const std::uint64_t tonic
   if (scale_hash_input == scale_hashes.at(std::to_underlying(Scales::Major)))
     std::println(stdout, "{:s}", return_scale_value(major_keys, scale_hash_input, tonic_hash_input));
   else if (scale_hash_input == scale_hashes.at(std::to_underlying(Scales::Dorian)))
+    std::println(stdout, "{:s}", return_scale_value(dorian_keys, scale_hash_input, tonic_hash_input));
+  else if (scale_hash_input == scale_hashes.at(std::to_underlying(Scales::Phrygian)))
     std::println(stdout, "{:s}", return_scale_value(dorian_keys, scale_hash_input, tonic_hash_input));
   else if (scale_hash_input == scale_hashes.at(std::to_underlying(Scales::Minor)))
     std::println(stdout, "{:s}", return_scale_value(minor_keys, scale_hash_input, tonic_hash_input));
