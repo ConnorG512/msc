3a9b622ce47abde25deb0000cc4d1b798e54ad5f
Connor Garey <connorgarey1999@gmail.com>
2026-05-17 13:14:01 +0100

    Changes to search table.
    .

--- include/search.hpp
+++ include/search.hpp
@@ -14,14 +14,11 @@ struct SearchTable
 {
   template <std::size_t intervals>
   consteval SearchTable(const MSC::Key::Gen<intervals> &key, std::uint64_t hash, std::string_view override = {})
-      : key_output_{MSC::Key::generate_title_and_notes(key)}, jump_strings_{key.get_jump_names()},
-        chords_{MSC::Key::get_chords(std::string_view{key.generate_key()})}, arg_hash_{hash}
+      : final_buffer_{MSC::Key::generate_final_output(key)}
   {
   }
 
-  std::array<char, 64> key_output_{};
-  std::array<char, 64> jump_strings_{};
-  std::array<char, 64> chords_{};
+  std::array<char, 256> final_buffer_{};
   std::uint64_t arg_hash_{};
 };
 } // namespace MSC
