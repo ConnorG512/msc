9015baed043736f40a0ba10c7ff0dc0fb06b3296
Connor Garey <connorgarey1999@gmail.com>
2026-05-18 13:54:07 +0100

    Made a class with functions.

--- include/cli-utils.hpp
+++ include/cli-utils.hpp
@@ -2,12 +2,23 @@
 
 #include <array>
 #include <cstddef>
+#include <string_view>
+#include <utility>
 
 namespace MSC
 {
-template <std::size_t arg_size = 16, std::size_t desc_size = 32> struct CmdArg
+template <std::size_t arg_size = 16, std::size_t desc_size = 32> class CmdArg
 {
-  std::array<char, desc_size> description{};
-  std::array<char, arg_size> args{};
+  public:
+    CmdArg<arg_size, desc_size>(std::pair<std::string_view, std::string_view> args, std::string_view description);
+    
+    std::string_view get_arg() const noexcept;
+    std::string_view get_description() const noexcept;
+
+  private:
+    std::array<char, desc_size> description{};
+    std::array<char, arg_size> args{};
 };
 } // namespace MSC
+
+#include "cli-utils.ipp"
