86508ca5c8e73f0e6ce2dc489ba1f3db9292f9ac
Connor Garey <connorgarey1999@gmail.com>
2026-05-19 15:51:43 +0100

    Changed std::array to to_array.

--- include/chromatic-scales.hpp
+++ include/chromatic-scales.hpp
@@ -5,8 +5,8 @@
 
 namespace MSC::ChromaticScales
 {
-constexpr inline std::array<std::string_view, 12> standard_sharp{"C",  "C#", "D",  "D#", "E",  "F",
-                                                                       "F#", "G",  "G#", "A",  "A#", "B"};
-constexpr inline std::array<std::string_view, 12> standard_flat{"C",  "Db", "D",  "Eb", "E",  "F",
-                                                                      "Gb", "G",  "Ab", "A",  "Bb", "B"};
+constexpr inline auto standard_sharp =
+    std::to_array<std::string_view>({"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"});
+constexpr inline auto standard_flat =
+    std::to_array<std::string_view>({"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"});
 } // namespace MSC::ChromaticScales
