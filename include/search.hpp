c277673788894f489b5c8ecbc61d841621a47e0b
Connor Garey <connorgarey1999@gmail.com>
2026-05-20 14:43:21 +0100

    Added scale and and changed arg hash to tonic_hash.

--- include/search.hpp
+++ include/search.hpp
@@ -14,12 +14,13 @@ void search(const std::uint64_t scale_hash, const std::uint64_t tonic_hash);
 struct SearchTable
 {
   template <std::size_t intervals>
-  consteval SearchTable(const MSC::Key::Gen<intervals> &key, std::uint64_t hash, std::string_view override = {})
-      : final_buffer_{MSC::Key::generate_final_output(key)}, arg_hash_{hash}
+  consteval SearchTable(const MSC::Key::Gen<intervals> &key, std::uint64_t scale_hash, std::uint64_t tonic_hash, std::string_view override = {})
+      : final_buffer_{MSC::Key::generate_final_output(key)}, scale_hash_{scale_hash}, tonic_hash_{tonic_hash}
   {
   }
 
   std::array<char, 512> final_buffer_{};
-  std::uint64_t arg_hash_{};
+  std::uint64_t scale_hash_{};
+  std::uint64_t tonic_hash_{};
 };
 } // namespace MSC
