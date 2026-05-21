045f37781ec45769257fb5e4805fd12af4f65367
Connor Garey <connorgarey1999@gmail.com>
2026-05-22 00:30:58 +0100

    Made generate chord a seperate lambda

--- include/chord-policies.hpp
+++ include/chord-policies.hpp
@@ -2,26 +2,23 @@
 
 #include "string_append.hpp"
 
+#include <cstdint>
+#include <initializer_list>
 #include <ranges>
+#include <string>
 #include <string_view>
 #include <vector>
 
 namespace MSC::Key
 {
-inline constexpr auto standard_chord = [](std::string_view key)
-{
-  std::string_view current_key{key.data(), key.find('\0')};
-
-  auto notes = current_key | std::views::split(' ') |
-               std::views::transform([](auto &&note_str) { return std::string_view(note_str); }) |
-               std::ranges::to<std::vector<std::string_view>>();
-
+inline constexpr auto generate_chord = [](const std::vector<std::string_view>& notes, const std::vector<std::int8_t>& jumps = {2,4}) -> std::string {
+  
   std::string output{};
-  for (const auto [index, note] : notes | std::views::enumerate)
+  for(const auto &[index, note] : notes | std::views::enumerate)
   {
     const auto root{index};
-    const auto third{(index + 2) % notes.size()};
-    const auto fith{(index + 4) % notes.size()};
+    const auto third{(index + jumps.at(0)) % notes.size()};
+    const auto fith{(index + jumps.at(1)) % notes.size()};
 
     output += "\t";
     output += note;
@@ -32,6 +29,18 @@ inline constexpr auto standard_chord = [](std::string_view key)
     output += "\n";
   }
 
+  return output;
+};
+
+inline constexpr auto standard_chord = [](std::string_view key)
+{
+  std::string_view current_key{key.data(), key.find('\0')};
+
+  auto notes = current_key | std::views::split(' ') |
+               std::views::transform([](auto &&note_str) { return std::string_view(note_str); }) |
+               std::ranges::to<std::vector<std::string_view>>();
+
+  std::string output{generate_chord(notes)};
   return append_strings_to_buffer<128>({{output.data(), output.size()}});
 };
 
@@ -43,22 +52,7 @@ inline constexpr auto pentatonic_chord = [](std::string_view key)
                std::views::transform([](auto &&note_str) { return std::string_view(note_str); }) |
                std::ranges::to<std::vector<std::string_view>>();
 
-  std::string output{};
-  for (const auto [index, note] : notes | std::views::enumerate)
-  {
-    const auto root{index};
-    const auto third{(index + 2) % notes.size()};
-    const auto fith{(index + 4) % notes.size()};
-
-    output += "\t";
-    output += note;
-    output += "-";
-    output += notes.at(third);
-    output += "-";
-    output += notes.at(fith);
-    output += "\n";
-  }
-
+  std::string output{generate_chord(notes)};
   return append_strings_to_buffer<128>(
       {"Consider using the Major / Minor of the derived key for harmoy.\n", {output.data(), output.size()}});
 };
