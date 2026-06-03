00cab2a10ee1126bfb953296cba3c3d9c04bc63c
Connor Garey <connorgarey1999@gmail.com>
2026-06-03 13:25:37 +0100

    Created a scales title.

--- include/scales.hpp
+++ include/scales.hpp
@@ -1,6 +1,10 @@
 #pragma once 
 
 #include "enum-to-string.hpp"
+#include <algorithm>
+#include <array>
+#include <ranges>
+#include <string_view>
 
 namespace MSC {
   enum class ScaleNames {
@@ -18,4 +22,20 @@ namespace MSC {
   };
 
   inline constexpr auto scale_names_ {MSC::enum_to_view<ScaleNames>()};
+  inline constexpr auto scale_titles = [&scale_names_](){
+    std::array<std::array<char, 32>, scale_names_.size()> buffers {};
+    std::ranges::transform(scale_names_, std::ranges::begin(buffers), [&](std::string_view scale_title){
+          std::array<char, 32> buffer {};
+          std::ranges::copy(scale_title, std::ranges::begin(buffer));
+          buffer.at(scale_title.size()) = '\0';
+
+          for(char &c : buffer | std::views::take(scale_title.size()))
+          {
+            if(c == '_')
+              c = ' ';
+          }
+          return buffer;
+        });
+    return buffers;
+  }();
 }
